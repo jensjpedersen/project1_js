a753afdcc570300b1e0559edf8433c6046ef7110
Jens Junker Pedersen <jens.junker.pedersen@gmail.com>
2023-11-08 14:46:36 +0100

    Oppgave 1

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 
-console.log('hei')
+
+
+
+let arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+console.log(arr.length)
+
+
+
+
+console.log(arr.pop())
+
+
+// arr.push = add to the end of the array and return the new length of the array
+console.log(arr.push('j'))
+
+
+
+console.log("Remove index 5 og index 7")
+console.log(arr.splice(5,1))
+console.log(arr.length)
+console.log(arr.splice(7,1))
+console.log(arr.length)
+
+
+console.log(`Fjernet index 5 og 7: ${arr}`)
+console.assert(arr.length === 8)
